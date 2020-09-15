903e7c4c146dbf2dfbc228bdf95968c97ed9a89e
Digidelphine <gaspardelphine01@gmail.com>
2020-09-15 10:29:00 +0200

    add change about

--- book2020/src/composants/About/About.js
+++ book2020/src/composants/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './About.css'
 import img from '../../img/avatar.jpg'
+import { Link } from 'react-router-dom'
 
 function About() {
   return (
@@ -13,15 +14,14 @@ function About() {
             </div>
             <div className="descriptif">
                 <h2>Bienvenue!</h2>
-                <p>“ Etudiante en marketing digital et UX design à Hetic, 
+                <p>“Etudiante en marketing digital et UX design à Hetic, 
                     ce programme m’a permit de connaitre l’ensemble des métiers
                     du web. Je porte aujourd’hui un intérêt tout particulier pour
                     l’UX/UI.Je souhaite intégrer une entreprise me permettant 
                     d’enrichir mes compétences dans ce domaine.”
                 </p>
-                <p className="citation">“ Seul n’a de valeur que le travail réalisé en profondeur dans 
-                    un engagement total et dans une lutte où l’on se livre tout 
-                    entier ” <br/>Werner Bishop
+                <p className="citation">
+                    “Je suis actuellement à la recherche d'une alternance dans le design web à partir de janvier 2021, n'hésitez pas à me <Link className="contact" to="/contact"> contacter </Link>"
                 </p>
             </div>
         </div>
